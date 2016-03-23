4a0c14680d966f8b548bc489f3dd483ae85bf7f3
Nakul Pathak <nakulpathak3@hotmail.com>
2016-03-22 22:49:02 -0400

    Messages in form are logged to console

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+  console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+
 });
 
 http.listen(3000, function(){
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
