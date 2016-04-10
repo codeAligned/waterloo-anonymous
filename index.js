6b0a836e03bd5832bc15d50e140a816cdecc7e71
Nakul Pathak <nakulpathak3@hotmail.com>
2016-04-10 17:20:51 -0400

    Random huge changes

--- index.js
+++ index.js
@@ -2,25 +2,66 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var queue = [];    // list of sockets waiting for peers
+var rooms = {};    // map socket.id => room
+var names = {};    // map socket.id => name
+var allUsers = {}; // map socket.id => socket
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  
-  var clients = io.sockets.sockets;
-  console.log(clients);
+var findPeerForLoneSocket = function(socket) {
+    // this is place for possibly some extensive logic
+    // which can involve preventing two people pairing multiple times
+    if (queue) {
+        // somebody is in queue, pair them!
+        var peer = queue.pop();
+        var room = socket.id + '#' + peer.id;
+        // join them both
+        peer.join(room);
+        socket.join(room);
+        // register rooms to their names
+        rooms[peer.id] = room;
+        rooms[socket.id] = room;
+        // exchange names between the two of them and start the chat
+        peer.emit('chat start', {'name': names[socket.id], 'room':room});
+        socket.emit('chat start', {'name': names[peer.id], 'room':room});
+    } else {
+        // queue is empty, add our lone socket
+        queue.push(socket);
+    }
+}
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-
+io.on('connection', function (socket) {
+    console.log('User '+socket.id + ' connected');
+    socket.on('login', function (data) {
+        names[socket.id] = data.username;
+        allUsers[socket.id] = socket;
+        // now check if sb is in queue
+        findPeerForLoneSocket(socket);
+    });
+    socket.on('message', function (data) {
+        var room = rooms[socket.id];
+        socket.broadcast.to(room).emit('message', data);
+    });
+    socket.on('leave room', function () {
+        var room = rooms[socket.id];
+        socket.broadcast.to(room).emit('chat end');
+        var peerID = room.split('#');
+        peerID = peerID[0] === socket.id ? peerID[1] : peerID[0];
+        // add both current and peer to the queue
+        findPeerForLoneSocket(allUsers[peerID]);
+        findPeerForLoneSocket(socket);
+    });
+    socket.on('disconnect', function () {
+        var room = rooms[socket.id];
+        socket.broadcast.to(room).emit('chat end');
+        var peerID = room.split('#');
+        peerID = peerID[0] === socket.id ? peerID[1] : peerID[0];
+        // current socket left, add the other one to the queue
+        findPeerForLoneSocket(allUsers[peerID]);
+    });
 });
 
 http.listen(process.env.PORT || 3000, function(){
